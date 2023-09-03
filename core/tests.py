4815987b053c4669eb03cd1d519e00b5f200c461
Felipe V Assunção <fassuncao368@gmail.com>
2023-09-03 16:11:35 -0400

    Executando teste da branch para realizar o merge

--- core/tests.py
+++ core/tests.py
@@ -5,5 +5,5 @@ class HelloWorldTestCase(unittest.TestCase):
 
     def test_hello_world(self):
         print("\n\nManutenção e Integração - Aluá P&D")
-        print("\nBranch pronta para ser mergeada\n")
+        print("Branch pronta para ser mergeada\n")
         assert True
