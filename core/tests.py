9c91fe833402857a927ba6264ec21b29c69bbf03
Felipe V Assunção <fassuncao368@gmail.com>
2023-09-03 15:46:08 -0400

    testando migrations

--- core/tests.py
+++ core/tests.py
@@ -4,5 +4,5 @@ import unittest
 class HelloWorldTestCase(unittest.TestCase):
 
     def test_hello_world(self):
-        print("\nManutenção e Integração - Aluá Software Development Institute\n")
+        print("\nManutenção e Integração - Aluá P&D\n")
         assert True
